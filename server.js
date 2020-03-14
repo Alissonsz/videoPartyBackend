4c02f9037c1e7cd09b52460a203ac43d9c52c538
Alissonsz <38736666+Alissonsz@users.noreply.github.com>
2020-03-14 19:25:37 -0300

    Finished initial chat implementation

--- server.js
+++ server.js
@@ -5,8 +5,27 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+var activeRooms = new Object();
+
+var events = new Object();
+
 io.on('connection', socket=> {
     console.log(socket.handshake.url);
+    socket.on('entryRoom', data => {
+        socket.join(data.roomURL);
+        
+        let event = {type: 'join', message: "entou na sala", userName: data.userName};
+        if(data.roomURL in events == false) events[data.roomURL] = [];
+        events[data.roomURL].push(event);
+        io.to(data.roomURL).emit('newEvent', event);
+        
+    });
+
+    socket.on('newMessage', data => {
+        console.log(`Received message from ${data.userName} : ${data.message}`);
+        let event = {type: 'message', message: data.message, userName: data.userName};
+        io.to(data.roomURL).emit('newEvent', event);
+    });
     
 });
 
